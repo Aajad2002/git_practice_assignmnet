7fa2475a1dfeee407bef333d59eb91addca6477b
= <aajad@gmail.com>
2022-11-02 21:43:05 +0530

    code fixed by member 1

--- prime.js
+++ prime.js
@@ -1,12 +1,12 @@
-let number=17
+
 function checkprime(number){
-    let count=0;
+    let factors=0;
     for(let i=1;i<=number;i++){
         if(number%i==0){
-            count++;
+            factors++;
         }
     }
-    if(count==0){
+    if(factors==2){
         console.log("prime")
     }else{
         console.log("not prime")
